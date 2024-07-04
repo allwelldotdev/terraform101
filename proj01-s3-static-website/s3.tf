aadfc088ec83a40d8b5de9dbbaecf63d94f7774c
Allwell Agwu-Okoro <allwell@allwell.dev>
2024-07-04 14:40:04 +0100

    deploy website file content to s3 bucket

--- proj01-s3-static-website/s3.tf
+++ proj01-s3-static-website/s3.tf
@@ -32,14 +32,79 @@ resource "aws_s3_bucket_policy" "static_website_public_read" {
   depends_on = [aws_s3_bucket.static_website, aws_s3_bucket_public_access_block.static_website]
 }
 
-# resource "aws_s3_bucket_website_configuration" "static_website" {
-#   bucket = aws_s3_bucket.static_website.id
+# Set s3 bucket to static website, select index and error doc
 
-#   index_document {
-#     suffix = "index.html"
-#   }
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
 
-#   error_document {
-#     key = "index.html"
-#   }
-# }
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# Deploy index.html file
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "index.html"
+  source       = "build/index.html"
+  etag         = filemd5("build/index.html")
+  content_type = "text/html"
+}
+
+# Deploy style.css file
+
+resource "aws_s3_object" "style_css" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "style.css"
+  source       = "build/style.css"
+  etag         = filemd5("build/style.css")
+  content_type = "text/css"
+}
+
+# Deploy img/ folder
+
+resource "aws_s3_object" "img_dir" {
+  bucket = aws_s3_bucket.static_website.id
+  key    = "img/"
+  source = "build/img/"
+}
+
+# Deploy files in img/ folder...
+
+resource "aws_s3_object" "img_dropdown" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "img/dropdown.svg"
+  source       = "build/img/dropdown.svg"
+  etag         = filemd5("build/img/dropdown.svg")
+  content_type = "img/svg"
+}
+
+resource "aws_s3_object" "img_eye" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "img/eye.svg"
+  source       = "build/img/eye.svg"
+  etag         = filemd5("build/img/eye.svg")
+  content_type = "img/svg"
+}
+
+resource "aws_s3_object" "img_logo" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "img/logo.svg"
+  source       = "build/img/logo.svg"
+  etag         = filemd5("build/img/logo.svg")
+  content_type = "img/svg"
+}
+
+resource "aws_s3_object" "img_date" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "img/date.svg"
+  source       = "build/img/date.svg"
+  etag         = filemd5("build/img/date.svg")
+  content_type = "img/svg"
+}
+
